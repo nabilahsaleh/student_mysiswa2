91caa2b4a4da114bc00e0a142dead1ed8eb4a859
nabilahsaleh <nabilahsalzu@gmail.com>
2024-09-27 16:45:23 +0800

    favicon

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MySiswa Admin',
+      title: 'MySiswa',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
